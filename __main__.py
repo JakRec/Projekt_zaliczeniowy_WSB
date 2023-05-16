6f7d680092327d53fc818319f6399ae46ad10537
JakRec <rechjakub@gmail.com>
2023-05-16 19:19:07 +0200

    added test creating account

--- __main__.py
+++ __main__.py
@@ -3,10 +3,12 @@
 # wczytanie bibliotek
 import unittest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+
+# from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions as EC
+
+# from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from time import sleep
 
@@ -16,25 +18,41 @@ class TestClass(unittest.TestCase):
         self.driver = webdriver.Chrome()
         self.action = webdriver.ActionChains(self.driver)
         self.wait = WebDriverWait(self.driver, 5)
+        self.driver.implicitly_wait(5)
 
     def test_website_opening(self):
         driver = self.driver
         driver.get("https://magento.softwaretestingboard.com/")
         assert driver.title == "Home Page"
 
-    def test_ceating_account(self):
+    def test_ceating_account_positive(self):
         driver = self.driver
-        action = self.action
-        wait = self.wait
         driver.get("https://magento.softwaretestingboard.com/")
         el1 = driver.find_element(
             By.XPATH, "/html/body/div[1]/header/div[1]/div/ul/li[3]/a"
         )
         # KLIKANIE ROZWIAZAC
-        action.click(el1)
-        driver.web
-        wait(4)
-        assert driver.title == "Create New Customer Account"
+        el1.click()
+        # assert driver.title == "Create New Customer Account"
+        el2 = driver.find_element(By.XPATH, '//input[@id="firstname"]')
+        el2.send_keys("John")
+        el3 = driver.find_element(By.XPATH, '//input[@id="lastname"]')
+        el3.send_keys("Doe")
+        el4 = driver.find_element(By.XPATH, '//input[@id="email_address"]')
+        el4.send_keys("johndoe@xye.com")
+        driver.find_element(By.XPATH, '//input[@id="password"]').send_keys(
+            "VeryDifficultPassword123"
+        )
+        driver.find_element(By.XPATH, '//input[@id="password-confirmation"]').send_keys(
+            "VeryDifficultPassword123"
+        )
+        driver.find_element(
+            By.XPATH, '//*[@id="form-validate"]/div/div[1]/button'
+        ).click()
+        assert driver.find_element(
+            By.XPATH,
+            '//div[@data-bind="html: $parent.prepareMessageForHtml(message.text)"]',
+        )
 
 
 def tearDown(self):
@@ -42,4 +60,4 @@ def tearDown(self):
 
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity=2)
