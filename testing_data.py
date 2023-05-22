e1b0a24c5d768714c5f5ac5e2a18e88872cd9830
JakRec <rechjakub@gmail.com>
2023-05-22 21:00:33 +0200

    adding faker

---
+++ testing_data.py
@@ -0,0 +1,16 @@
+from faker import Faker
+
+fake = Faker()
+fake1 = Faker.provid
+
+
+def test_fistname(self):
+    return fake.name
+
+
+def test_surname():
+    return fake.surname()
+
+
+def test_password():
+    return fake.password()
