016bd997da6d03375a14854578e7b6bd874b95c1
JakRec <rechjakub@gmail.com>
2023-05-31 10:48:06 +0200

    all tests in methods

--- tests_code_create_account.py
+++ tests_code_create_account.py
@@ -5,7 +5,7 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-# from selenium.webdriver.support import expected_conditions as EC biblioteka na ten moment nie uzywana
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.select import Select
 from time import sleep
@@ -26,25 +26,37 @@ def sign_in_page_sign_in_button_click(self):
 def login_to_account(self):
     driver = self.driver
     goto_main_page(self)
-    print(
-        driver.find_element(By.XPATH, self.locators.main_page_welcome_message_id).text
-    )
-    if (
-        driver.find_element(By.XPATH, self.locators.main_page_welcome_message_id).text
-        == "Welcome, John Doe!"
-    ):
+    if check_if_John_Doe_logged_in(self):
         print("Użytkownik zalogowany, kontynuacja")
     else:
         print("Użytkownik niezalogowany, logowanie")
-        driver.find_element(By.XPATH, self.locators.main_page_sign_in_button_id).click()
-        driver.find_element(By.XPATH, self.locators.sign_in_page_mail_id).send_keys(
-            self.dane.account_data_valid("mail")
-        )
-        driver.find_element(By.XPATH, self.locators.sign_in_page_password_id).send_keys(
-            self.dane.account_data_valid("password")
-        )
+        goto_sign_in_page(self)
+        sign_in_page_fill_in_mail(self)
+        sign_in_page_fill_in_password(self)
         sign_in_page_sign_in_button_click(self)
     sleep(2)
+
+
+def check_if_John_Doe_logged_in(self):
+    driver = self.driver
+    return (
+        driver.find_element(By.XPATH, self.locators.main_page_welcome_message_id).text
+        == "Welcome, John Doe!"
+    )
+
+
+def sign_in_page_fill_in_mail(self):
+    driver = self.driver
+    driver.find_element(By.XPATH, self.locators.sign_in_page_mail_id).send_keys(
+        self.dane.account_data_valid("mail")
+    )
+
+
+def sign_in_page_fill_in_password(self):
+    driver = self.driver
+    driver.find_element(By.XPATH, self.locators.sign_in_page_password_id).send_keys(
+        self.dane.account_data_valid("password")
+    )
 
 
 def goto_account_data(self):
@@ -64,6 +76,11 @@ def goto_shipping_adress_page(self):
     ).click()
 
 
+def goto_sign_in_page(self):
+    driver = self.driver
+    driver.find_element(By.XPATH, self.locators.main_page_sign_in_button_id).click()
+
+
 def clean_cart(self):
     driver = self.driver
     sleep(2)
@@ -81,7 +98,7 @@ def clean_cart(self):
 def goto_create_account_page(self):
     driver = self.driver
     driver.find_element(
-        By.XPATH, "/html/body/div[1]/header/div[1]/div/ul/li[3]/a"
+        By.XPATH, self.locators.main_page_create_account_button_id
     ).click()
 
 
@@ -106,6 +123,13 @@ def account_page_fill_mail(self):
     )
 
 
+def account_page_fill_mail_existing_and_valid(self):
+    driver = self.driver
+    driver.find_element(By.XPATH, self.locators.register_mail_id).send_keys(
+        self.dane.account_data_valid("mail")
+    )
+
+
 def account_page_fill_password(self, type):
     driver = self.driver
     driver.find_element(By.XPATH, self.locators.register_password_id).send_keys(
@@ -122,7 +146,7 @@ def account_page_fill_repeat_password(self, type):
 
 def account_page_submit_button_click(self):
     driver = self.driver
-    driver.find_element(By.XPATH, '//*[@id="form-validate"]/div/div[1]/button').click()
+    driver.find_element(By.XPATH, self.locators.account_page_submit_button_id).click()
 
 
 def account_page_fill_password_too_short(self):
@@ -132,7 +156,9 @@ def account_page_fill_password_too_short(self):
         sleep(0.5)
         password_length += 1
         if (
-            driver.find_element(By.XPATH, '//*[@id="password-error"]').text
+            driver.find_element(
+                By.XPATH, self.locators.create_account_password_error_id
+            ).text
             == "Minimum length of this field must be equal or greater than 8 symbols. Leading and trailing spaces will be ignored."
         ):
             continue
@@ -174,7 +200,9 @@ def account_page_no_mail_assert(self):
 def account_page_password_no_special_char_assert(self):
     driver = self.driver
     assert (
-        driver.find_element(By.XPATH, '//*[@id="password-error"]').text
+        driver.find_element(
+            By.XPATH, self.locators.create_account_password_error_id
+        ).text
         == "Minimum of different classes of characters in password is 3. Classes of characters: Lower Case, Upper Case, Digits, Special Characters."
     )
 
@@ -182,7 +210,9 @@ def account_page_password_no_special_char_assert(self):
 def account_page_password_not_match_assert(self):
     driver = self.driver
     assert (
-        driver.find_element(By.XPATH, '//*[@id="password-confirmation-error"]').text
+        driver.find_element(
+            By.XPATH, self.locators.create_account_repeat_password_error_id
+        ).text
         == "Please enter the same value again."
     )
 
@@ -190,7 +220,9 @@ def account_page_password_not_match_assert(self):
 def account_page_password_weak_assert(self):
     driver = self.driver
     assert (
-        driver.find_element(By.XPATH, '//*[@id="password-strength-meter-label"]').text
+        driver.find_element(
+            By.XPATH, self.locators.create_account_password_strength_meter_label_id
+        ).text
         == "Weak"
     )
 
@@ -198,16 +230,62 @@ def account_page_password_weak_assert(self):
 def account_page_password_medium_assert(self):
     driver = self.driver
     assert (
-        driver.find_element(By.XPATH, '//*[@id="password-strength-meter-label"]').text
+        driver.find_element(
+            By.XPATH, self.locators.create_account_password_strength_meter_label_id
+        ).text
         == "Medium"
     )
 
 
-class TestAccountCreatingForm(unittest.TestCase):
-    driver = webdriver.Chrome()
+def account_page_password_strong_assert(self):
+    driver = self.driver
+    assert (
+        driver.find_element(
+            By.XPATH, self.locators.create_account_password_strength_meter_label_id
+        ).text
+        == "Strong"
+    )
 
+
+def account_page_password_very_hard_assert(self):
+    driver = self.driver
+    assert (
+        driver.find_element(
+            By.XPATH, self.locators.create_account_password_strength_meter_label_id
+        ).text
+        == "Very Strong"
+    )
+
+
+def account_page_create_account_but_mail_already_existing_assert(self):
+    driver = self.driver
+    assert EC.visibility_of(
+        driver.find_element(
+            By.XPATH, self.locators.create_account_mail__already_existing_id
+        )
+    )
+
+
+def sign_in_to_existing_account_assert(self):
+    driver = self.driver
+    assert (
+        driver.find_element(By.XPATH, self.locators.main_page_welcome_message_id).text
+        == "Welcome, "
+        + self.dane.account_data_valid("name")
+        + " "
+        + self.dane.account_data_valid("surname")
+        + "!"
+    )
+
+
+def main_page_title_assert(self):
+    driver = self.driver
+    assert driver.title == "Home Page"
+
+
+class TestAccountCreatingForm(unittest.TestCase):
     def setUp(self):
-        # self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome()
         self.action = webdriver.ActionChains(self.driver)
         self.wait = WebDriverWait(self.driver, 2)
         self.driver.implicitly_wait(5)
@@ -217,7 +295,7 @@ class TestAccountCreatingForm(unittest.TestCase):
     def test_website_opening(self):
         driver = self.driver
         goto_main_page(self)
-        assert driver.title == "Home Page"
+        main_page_title_assert(self)
 
     def test_creating_account_no_name(self):
         driver = self.driver
@@ -270,7 +348,7 @@ class TestAccountCreatingForm(unittest.TestCase):
         account_page_fill_lastname(self)
         account_page_fill_mail(self)
         account_page_fill_password(self, "no special char")
-        sleep(2)
+        sleep(1)
         account_page_password_no_special_char_assert(self)
 
     def test_creating_account_password_not_match(self):
@@ -283,7 +361,7 @@ class TestAccountCreatingForm(unittest.TestCase):
         account_page_fill_password(self, "ok")
         account_page_fill_repeat_password(self, "ok different")
         account_page_submit_button_click(self)
-        sleep(2)
+        sleep(1)
         account_page_password_not_match_assert(self)
 
     def test_creating_account_password_weak(self):
@@ -294,7 +372,7 @@ class TestAccountCreatingForm(unittest.TestCase):
         account_page_fill_lastname(self)
         account_page_fill_mail(self)
         account_page_fill_password(self, "weak")
-        sleep(2)
+        sleep(1)
         account_page_password_weak_assert(self)
 
     def test_creating_account_password_medium(self):
@@ -305,7 +383,7 @@ class TestAccountCreatingForm(unittest.TestCase):
         account_page_fill_lastname(self)
         account_page_fill_mail(self)
         account_page_fill_password(self, "medium")
-        sleep(2)
+        sleep(1)
         account_page_password_medium_assert(self)
 
     def test_creating_account_password_strong(self):
@@ -316,13 +394,8 @@ class TestAccountCreatingForm(unittest.TestCase):
         account_page_fill_lastname(self)
         account_page_fill_mail(self)
         account_page_fill_password(self, "strong")
-        sleep(2)
-        assert (
-            driver.find_element(
-                By.XPATH, '//*[@id="password-strength-meter-label"]'
-            ).text
-            == "Strong"
-        )
+        sleep(1)
+        account_page_password_strong_assert(self)
 
     def test_creating_account_password_very_strong(self):
         driver = self.driver
@@ -331,42 +404,32 @@ class TestAccountCreatingForm(unittest.TestCase):
         account_page_fill_firstname(self)
         account_page_fill_lastname(self)
         account_page_fill_mail(self)
-        driver.find_element(By.XPATH, self.locators.register_password_id).send_keys(
-            self.dane.test_password("very strong")
-        )
-        sleep(2)
-        assert (
-            driver.find_element(
-                By.XPATH, '//*[@id="password-strength-meter-label"]'
-            ).text
-            == "Very Strong"
-        )
+        account_page_fill_password(self, "very strong")
+        sleep(1)
+        account_page_password_very_hard_assert(self)
 
-    def test_login_to_existing_account(self):
+    def test_creating_account_but_mail_already_existing(self):
         driver = self.driver
         goto_main_page(self)
-        driver.find_element(
-            By.XPATH, "/html/body/div[1]/header/div[1]/div/ul/li[2]/a"
-        ).click()
-        driver.find_element(By.XPATH, self.locators.sign_in_page_mail_id).send_keys(
-            self.dane.account_data_valid("mail")
-        )
-        driver.find_element(By.XPATH, self.locators.sign_in_page_password_id).send_keys(
-            self.dane.account_data_valid("password")
-        )
+        goto_create_account_page(self)
+        account_page_fill_firstname(self)
+        account_page_fill_lastname(self)
+        account_page_fill_mail_existing_and_valid(self)
+        account_page_fill_password(self, "ok")
+        account_page_fill_repeat_password(self, "ok")
+        account_page_submit_button_click(self)
+        sleep(2)
+        account_page_create_account_but_mail_already_existing_assert(self)
+
+    def test_sign_in_to_existing_account(self):
+        driver = self.driver
+        goto_main_page(self)
+        goto_sign_in_page(self)
+        sign_in_page_fill_in_mail(self)
+        sign_in_page_fill_in_password(self)
         sign_in_page_sign_in_button_click(self)
         sleep(4)
-
-        assert (
-            driver.find_element(
-                By.XPATH, self.locators.main_page_welcome_message_id
-            ).text
-            == "Welcome, "
-            + self.dane.account_data_valid("name")
-            + " "
-            + self.dane.account_data_valid("surname")
-            + "!"
-        )
+        sign_in_to_existing_account_assert(self)
 
 
 def tearDown(self):
@@ -376,27 +439,16 @@ def tearDown(self):
 if __name__ == "__main__":
     unittest.main(verbosity=2)
 
-
-"""    def test_ceating_account_positive(self):
+"""  Due to assertion method, this will be only positive when creating the account for first time
+    def test_creating_account_positive(self):
         goto_main_page(self)
         goto_create_account_page(self)
-        el2 = driver.find_element(By.XPATH, '//input[@id="firstname"]')
-        el2.send_keys("John")
-        el3 = driver.find_element(By.XPATH, '//input[@id="lastname"]')
-        el3.send_keys("Doe")
-        el4 = driver.find_element(By.XPATH, '//input[@id="email_address"]')
-        el4.send_keys("johndoe@xyb.com")
-        driver.find_element(By.XPATH, '//input[@id="password"]').send_keys(
-            "VeryDifficultPassword123"
-        )
-        driver.find_element(By.XPATH, '//input[@id="password-confirmation"]').send_keys(
-            "VeryDifficultPassword123"
-        )
-        driver.find_element(
-            By.XPATH, '//*[@id="form-validate"]/div/div[1]/button'
-        ).click()
-        assert driver.find_element(
-            By.XPATH,
-            '//div[@data-bind="html: $parent.prepareMessageForHtml(message.text)"]',
-        )
+        account_page_fill_firstname(self)
+        account_page_fill_lastname(self)
+        account_page_fill_mail(self)
+        account_page_fill_password(self, "ok")
+        account_page_fill_repeat_password(self, "ok")
+        account_page_submit_button_click(self)
+        sleep(2)
+        check_if_John_Doe_logged_in(self)
 """
--- tests_site_locators.py
+++ tests_site_locators.py
@@ -6,8 +6,18 @@ register_password_id = '//input[@id="password"]'
 register_password_confirmation_id = '//input[@id="password-confirmation"]'
 main_page_welcome_message_id = "/html/body/div[1]/header/div[1]/div/ul/li[1]/span"
 main_page_sign_in_button_id = '//a[contains(text(), "Sign In")]'
+main_page_create_account_button_id = "/html/body/div[1]/header/div[1]/div/ul/li[3]/a"
 sign_in_page_mail_id = '//*[@id="email"]'
 sign_in_page_password_id = '//*[@id="pass"]'
 create_account_firstname_error_id = '//*[@id="firstname-error"]'
 create_account_lastname_error_id = '//*[@id="lastname-error"]'
 create_account_mail_error_id = '//*[@id="email_address-error"]'
+create_account_password_error_id = '//*[@id="password-error"]'
+create_account_repeat_password_error_id = '//*[@id="password-confirmation-error"]'
+create_account_password_strength_meter_label_id = (
+    '//*[@id="password-strength-meter-label"]'
+)
+account_page_submit_button_id = '//button[@type="submit"][@title="Create an Account"]'
+create_account_mail__already_existing_id = (
+    '//*[@id="maincontent"]/div[2]/div[2]/div/div/div'
+)
